dd696c1a65b48018f7e1ee66806196a8b8f88340
Pedro Campos <pedrogcamposb@outlook.com>
2021-11-13 16:03:43 -0300

    Initial commit

---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,23 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  name: string;
+  email: string;
+}
+
+class CreateUserUseCase {
+  constructor(private usersRepository: IUsersRepository) { }
+
+  execute({ email, name }: IRequest): User {
+    const emailAlreadyExists = this.usersRepository.findByEmail(email);
+
+    if (emailAlreadyExists) {
+      throw new Error("User email already exists!");
+    }
+
+    return this.usersRepository.create({ email, name });
+  }
+}
+
+export { CreateUserUseCase };
